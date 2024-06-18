d250c0eaef638a6fc1f91bdbc31dd22f692fc65c
Mouniesh <iammounieshv@gmail.com>
2024-06-18 21:16:44 +0530

    y prop in animate variant and times in transition added

--- app/components/BasicsOfMotion.jsx
+++ app/components/BasicsOfMotion.jsx
@@ -23,19 +23,23 @@ const BasicsOfMotion = () => {
        { isVisible && <motion.div
         initial={{ 
           rotate:"0deg",
-          scale: 0
+          scale: 0,
+          y:0
          }}
         animate={{
           rotate:"360deg",
-          scale: 1
+          scale: 1,
+          y:[0, 150, -150, -150, 0]
         }}
         exit={{
           rotate:"0deg",
-          scale: 0
+          scale: 0,
+          y:0
         }}
         transition={{
           duration: 1,
-          ease: 'backOut'
+          ease: 'backOut',
+          times: [0, 0.25, 0.5, -0.85, 1]
         }}
         style={{
             width: '180px',

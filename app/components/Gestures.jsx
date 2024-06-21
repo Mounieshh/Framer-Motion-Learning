4a3f88458ced33e48513801e9963a0a7631a5755
Mouniesh <iammounieshv@gmail.com>
2024-06-21 22:48:55 +0530

    Gestures component added

---
+++ app/components/Gestures.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Gestures = () => {
+  return (
+    <>
+    <div 
+    style={{
+        display: 'grid',
+        placeContent: 'center',
+        height: '100vh',
+        gap: '0.8 rem'
+    }}
+    >
+
+    </div>
+    </>
+  )
+}
+
+export default Gestures

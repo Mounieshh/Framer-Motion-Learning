6a14acf1843b86af9214ecc7c0e29f70f3a9e8e7
Mouniesh <iammounieshv@gmail.com>
2024-06-26 22:28:01 +0530

    useInView hook from framer motion

--- app/components/View.jsx
+++ app/components/View.jsx
@@ -1,12 +1,23 @@
 'use client'
 
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useEffect, useRef } from 'react'
+import { motion, useInView } from 'framer-motion'
 
 const View = () => {
+
+     //Adding useRef Hook
+
+     const ref = useRef(null)
+     const isInView = useInView(ref, {once : true})
+
+     useEffect(() => {
+        console.log("Is In View -> ",isInView);
+
+     },[isInView])
   return (
     <>
     <div style={{height: '150vh'}}/>
+
     <motion.div
     style={{
         height: '100vh',
@@ -15,9 +26,18 @@ const View = () => {
     initial={{opacity: 0}}
     whileInView={{opacity: 1}}
     transition={{duration: 0.4}}
-    >
+    />
+    
 
-    </motion.div>
+
+    <div
+    ref={ref}
+    style={{
+        height: '100vh',
+        background: isInView ? 'blue' : 'red',
+        transition: '1s background'
+    }}
+    />
      </>
   )
 }

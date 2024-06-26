37c4559dea95816b20fd483ffa81a12f35fa02a7
Mouniesh <iammounieshv@gmail.com>
2024-06-26 22:12:53 +0530

    view based animations

--- app/components/View.jsx
+++ app/components/View.jsx
@@ -1,8 +1,24 @@
+'use client'
+
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const View = () => {
   return (
-    <div>View</div>
+    <>
+    <div style={{height: '150vh'}}/>
+    <motion.div
+    style={{
+        height: '100vh',
+        background:'black'
+    }}
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1}}
+    >
+
+    </motion.div>
+     </>
   )
 }
 

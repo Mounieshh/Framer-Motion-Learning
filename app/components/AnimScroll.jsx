3f94f2f88ef642047f489a135134cd20af177994
Mouniesh <iammounieshv@gmail.com>
2024-06-27 17:06:40 +0530

    setup modified

---
+++ app/components/AnimScroll.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const AnimScroll = () => {
+  return (
+    <>
+    <div>
+        <div 
+        className='max-w-3xl m-auto p-5'>
+            <p>
+                Lorem ipsum dolor sit amet 
+                consectetur adipisicing elit. Itaque nobis rerum praesentium placeat. Quia, quos omnis libero, saepe error numquam voluptates non 
+                rem obcaecati quam molestias facilis, perferendis ab cumque! Ea, totam quidem nisi et est temporibus adipisci culpa illum provident omnis vel tenetur minima vitae velit cum id minus, 
+                repellendus dicta, blanditiis eveniet ut. Dicta eaque illo aspernatur. Magni, ratione qui vero, illo neque facere tenetur, quo officia libero perspiciatis rerum consectetur maxime exercitationem voluptates a.
+                Placeat, voluptate, cumque in consequatur rerum facilis 
+                nam perferendis accusamus hic, quisquam magni aut eius quas iste? Error eum maiores praesentium ipsa excepturi!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet 
+                consectetur adipisicing elit. Itaque nobis rerum praesentium placeat. Quia, quos omnis libero, saepe error numquam voluptates non 
+                rem obcaecati quam molestias facilis, perferendis ab cumque! Ea, totam quidem nisi et est temporibus adipisci culpa illum provident omnis vel tenetur minima vitae velit cum id minus, 
+                repellendus dicta, blanditiis eveniet ut. Dicta eaque illo aspernatur. Magni, ratione qui vero, illo neque facere tenetur, quo officia libero perspiciatis rerum consectetur maxime exercitationem voluptates a.
+                Placeat, voluptate, cumque in consequatur rerum facilis 
+                nam perferendis accusamus hic, quisquam magni aut eius quas iste? Error eum maiores praesentium ipsa excepturi!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet 
+                consectetur adipisicing elit. Itaque nobis rerum praesentium placeat. Quia, quos omnis libero, saepe error numquam voluptates non 
+                rem obcaecati quam molestias facilis, perferendis ab cumque! Ea, totam quidem nisi et est temporibus adipisci culpa illum provident omnis vel tenetur minima vitae velit cum id minus, 
+                repellendus dicta, blanditiis eveniet ut. Dicta eaque illo aspernatur. Magni, ratione qui vero, illo neque facere tenetur, quo officia libero perspiciatis rerum consectetur maxime exercitationem voluptates a.
+                Placeat, voluptate, cumque in consequatur rerum facilis 
+                nam perferendis accusamus hic, quisquam magni aut eius quas iste? Error eum maiores praesentium ipsa excepturi!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet 
+                consectetur adipisicing elit. Itaque nobis rerum praesentium placeat. Quia, quos omnis libero, saepe error numquam voluptates non 
+                rem obcaecati quam molestias facilis, perferendis ab cumque! Ea, totam quidem nisi et est temporibus adipisci culpa illum provident omnis vel tenetur minima vitae velit cum id minus, 
+                repellendus dicta, blanditiis eveniet ut. Dicta eaque illo aspernatur. Magni, ratione qui vero, illo neque facere tenetur, quo officia libero perspiciatis rerum consectetur maxime exercitationem voluptates a.
+                Placeat, voluptate, cumque in consequatur rerum facilis 
+                nam perferendis accusamus hic, quisquam magni aut eius quas iste? Error eum maiores praesentium ipsa excepturi!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet 
+                consectetur adipisicing elit. Itaque nobis rerum praesentium placeat. Quia, quos omnis libero, saepe error numquam voluptates non 
+                rem obcaecati quam molestias facilis, perferendis ab cumque! Ea, totam quidem nisi et est temporibus adipisci culpa illum provident omnis vel tenetur minima vitae velit cum id minus, 
+                repellendus dicta, blanditiis eveniet ut. Dicta eaque illo aspernatur. Magni, ratione qui vero, illo neque facere tenetur, quo officia libero perspiciatis rerum consectetur maxime exercitationem voluptates a.
+                Placeat, voluptate, cumque in consequatur rerum facilis 
+                nam perferendis accusamus hic, quisquam magni aut eius quas iste? Error eum maiores praesentium ipsa excepturi!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet 
+                consectetur adipisicing elit. Itaque nobis rerum praesentium placeat. Quia, quos omnis libero, saepe error numquam voluptates non 
+                rem obcaecati quam molestias facilis, perferendis ab cumque! Ea, totam quidem nisi et est temporibus adipisci culpa illum provident omnis vel tenetur minima vitae velit cum id minus, 
+                repellendus dicta, blanditiis eveniet ut. Dicta eaque illo aspernatur. Magni, ratione qui vero, illo neque facere tenetur, quo officia libero perspiciatis rerum consectetur maxime exercitationem voluptates a.
+                Placeat, voluptate, cumque in consequatur rerum facilis 
+                nam perferendis accusamus hic, quisquam magni aut eius quas iste? Error eum maiores praesentium ipsa excepturi!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet 
+                consectetur adipisicing elit. Itaque nobis rerum praesentium placeat. Quia, quos omnis libero, saepe error numquam voluptates non 
+                rem obcaecati quam molestias facilis, perferendis ab cumque! Ea, totam quidem nisi et est temporibus adipisci culpa illum provident omnis vel tenetur minima vitae velit cum id minus, 
+                repellendus dicta, blanditiis eveniet ut. Dicta eaque illo aspernatur. Magni, ratione qui vero, illo neque facere tenetur, quo officia libero perspiciatis rerum consectetur maxime exercitationem voluptates a.
+                Placeat, voluptate, cumque in consequatur rerum facilis 
+                nam perferendis accusamus hic, quisquam magni aut eius quas iste? Error eum maiores praesentium ipsa excepturi!
+            </p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default AnimScroll
--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import BasicsOfMotion from './components/BasicsOfMotion'
 import Gestures from './components/Gestures'
 import AnimControls from './components/AnimControls'
 import View from './components/View'
+import AnimScroll from './components/AnimScroll'
 
 const page = () => {
   return (
@@ -14,6 +15,7 @@ const page = () => {
     <Gestures/>
     <AnimControls/>
     <View/>
+   <AnimScroll/>
     </>
   )
 }

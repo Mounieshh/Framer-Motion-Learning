cf2351a3e12c37d5f2060f1d6eee19293a02d8e3
Mouniesh <iammounieshv@gmail.com>
2024-06-27 17:34:37 +0530

    useTransform Hook

--- app/page.jsx
+++ app/page.jsx
@@ -7,19 +7,26 @@ import AnimControls from './components/AnimControls'
 import View from './components/View'
 import AnimScroll from './components/AnimScroll'
 
-import { motion, useScroll, useSpring } from 'framer-motion'
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion'
 
 const page = () => {
 
   const { scrollYProgress } = useScroll()
   const scaleX = useSpring(scrollYProgress)
+
+  const background = useTransform(
+    scrollYProgress,
+    [0, 1],
+    // ['rgb(87, 1, 245)', 'rgb(15, 233,5)']
+    ['#3456y5', '#1234e5']
+  )
   return (
     <>
 
     <motion.div
       style={{
       scaleX,
-      background: 'red',
+      background,
       transformOrigin: 'left',
       position: 'sticky',
       top: '0',

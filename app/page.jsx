bdeae7b8767c54c742f98df76950dc31e0e4dc24
Mouniesh <iammounieshv@gmail.com>
2024-06-27 17:21:03 +0530

    modified page.jsx for scroll animations

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+
+'use client'
 import React from 'react'
 import BasicsOfMotion from './components/BasicsOfMotion'
 import Gestures from './components/Gestures'
@@ -5,9 +7,25 @@ import AnimControls from './components/AnimControls'
 import View from './components/View'
 import AnimScroll from './components/AnimScroll'
 
+import { motion, useScroll } from 'framer-motion'
+
 const page = () => {
+
+  const {scrollYProgress} = useScroll()
   return (
     <>
+
+    <motion.div
+      style={{
+      scaleX: scrollYProgress,
+      background: 'red',
+      transformOrigin: 'left',
+      position: 'sticky',
+      top: '0',
+      width: '100%',
+      height: '20px'
+     }}
+      />
     <h3>
       Basics
     </h3>
